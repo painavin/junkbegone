9d2f24f4ea717674e6feaebd0ee68f4053d82963
Navin Pai <navin.pai@apple.com>
2026-07-08 18:05:08 -0400

    updated to 4 hrs.

--- az-function/src/functions/dailyCleanup.js
+++ az-function/src/functions/dailyCleanup.js
@@ -2,7 +2,7 @@ const { app } = require("@azure/functions");
 const { runCleanup } = require("../../graphClient");
 
 app.timer("dailyCleanup", {
-  schedule: "0 0 */1 * * *", // every hour
+  schedule: "0 0 */4 * * *", // every 4 hours
   handler: async (myTimer, context) => {
     try {
       const result = await runCleanup();
